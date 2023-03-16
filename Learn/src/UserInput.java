6449fabd00e4966a1f48249f75f1b8338e1f9526
smadi0x86 <smadixd@gmail.com>
2023-03-16 20:56:01 +0300

    Edited by: yousinator0x01

--- Learn/src/UserInput.java
+++ Learn/src/UserInput.java
@@ -23,17 +23,17 @@
 
     * Reading user input:
     ? 1 > We have to create a scanner object that will read the input from the console => Scanner scanner = new Scanner(System.in);.
-    ? 2 > You'll have create a varisble to store the user input.
+    ? 2 > You'll have create a variable to store the user input.
     ? 3 > You'll have to to use the "next" method for the scanner object to read the user input. => String name = scanner.nextLine();.
     ? 4 > The data type of the variable must natch the type of the method.
-    ? 5 > close the scanner object. => scanner.close();.
+    ? 5 > Close the scanner object. => scanner.close();.
 
     * The "next" method:
     ? For each data type there is a next method. => nextInt , nextLong..... .
     ? The data type of the variable must match the method. => int x = scanner.nextInt , long x = scanner.nextLong.
     ? For the "String" data type there are two methods:
-        ! next(): Reads only one word. It stops reading when a space is entered. 
-        ! nextLine(): Reads spaces with words. It stops reading ONLY when an "Enter" is entered. 
+        ! next(): Reads only one word. It stops reading when a space is entered.
+        ! nextLine(): Reads spaces with words. It stops reading ONLY when an "Enter" is entered.
 */
 
 //! ------------------------------------------------------ The Code ------------------------------------------------------ //
@@ -45,19 +45,14 @@ public class UserInput {
 
         Scanner scanner = new Scanner(System.in); // ? 1
 
-<<<<<<< HEAD
-        System.out.println("What is your name?"); // ? This is how you print to the console
-        String name = scanner.nextLine(); // ? This is how you read a line of input from the console
-=======
         System.out.println("What is your name?");
         String name = scanner.nextLine(); // ? 2 , 3 , 4
->>>>>>> 4eca749260fff66911433907743bd25716a3dfc0
 
         System.out.println("How old are you?");
         int age = scanner.nextInt();
 
         scanner.nextLine(); // ? This is to clear the buffer of the scanner object so that the next call to
-                            // nextLine() will not return an empty string
+                            // ? nextLine() will get the next line of input from the user.
 
         System.out.println("What is your favorite color?");
         String color = scanner.nextLine();
