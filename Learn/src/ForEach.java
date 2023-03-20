d7d753a28fef1d9e8fb05c9f0c4acef61c22c217
Yousinator <y.omusabeh@gmail.com>
2023-03-20 07:19:26 +0300

    Edited by: Yousinator

--- Learn/src/ForEach.java
+++ Learn/src/ForEach.java
@@ -7,39 +7,36 @@
 */
 
 /*
-    Explaining
+    Explaining For Each Loop
     ======================
-    !!!!! This file is yet to be documented !!!!!
-    
+    ! It is used to iterate through the elements of an array.
+    ! Difference between "For Each" and "For Loop" is that "For Each" doesn't require the index of the array to be accessed.
+
 */
 
 /*
     Explaining the code
     ======================
-    
+    ? 1 > The ":" means "in" and the variable "fruits" is the name of the variable that will hold the value of the current element.
+    ? 2 > We can also use the For Each loop to iterate through the elements of an ArrayList
+
+    * Refer to "ArrayList.java" to know more about ArrayLists.
+
 */
 
 import java.util.ArrayList;
 
 public class ForEach {
     public static void learn(String[] args) throws Exception {
-        // ! For Each Loop
-        // ! It is used to iterate through the elements of an array
-        // ! Difference between For Each and For Loop is that For Each doesn't require
-        // the index of the array to be accessed
-        // ! Less code, more readable but less flexible which means that we can't use it
-        // to modify the array
 
         String[] fruits = { "Apple", "Banana", "Orange" };
 
-        // ? The ":" means "in" and the variable fruit is the name of the variable that
-        // will hold the value of the current element
+        // ? 1
         for (String i : fruits) {
             System.out.println(i);
         }
 
-        // ? We can also use the For Each loop to iterate through the elements of an
-        // ArrayList
+        // ? 2
         ArrayList<String> sweets = new ArrayList<>();
 
         sweets.add("Chocolate");
