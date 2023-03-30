fd9d5f40c25a5959b7f48ec0e70abe3bf0851390
smadi0x86 <smadixd@gmail.com>
2023-03-30 13:26:33 +0300

    commit

--- Learn/src/OverloadedConstructors.java
+++ Learn/src/OverloadedConstructors.java
@@ -1,6 +1,7 @@
 public class OverloadedConstructors {
     public static void main(String[] args) {
-        // ! overloaded constructors = multiple within a class with the same name, but different parameters.
+        // ! overloaded constructors = multiple within a class with the same name, but
+        // different parameters.
         // ! name + parameters = signature
         // * Pizza.java and OverloadedConstructors.java are in the same folder
 
