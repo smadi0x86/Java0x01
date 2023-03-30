705a56e909dec873232b3ccd7e862cf8210de505
smadi0x86 <smadixd@gmail.com>
2023-03-30 13:21:11 +0300

    commit

--- Learn/src/OverloadedConstructors.java
+++ Learn/src/OverloadedConstructors.java
@@ -1,7 +1,9 @@
 public class OverloadedConstructors {
     public static void main(String[] args) {
-        // !
+        // ! overloaded constructors = multiple within a class with the same name, but different parameters.
+        // ! name + parameters = signature
 
 
-}
+
+    }
 }
