0389f66448a954371cbdbd8b077ae67fad9f761c
Yousinator <y.omusabeh@gmail.com>
2023-04-09 07:11:28 +0300

    Edited by: Yousinator

--- Learn/src/Trucks.java
+++ Learn/src/Trucks.java
@@ -1,5 +1,5 @@
+// * This works simultaneously with the "Trucks.java" and "ObjectPassing.java" files (We want to park the trucks in garage).
 public class Trucks {
-    // * This works simultaneously with the "Trucks.java" and "ObjectPassing.java" files (We want to park the trucks in garage).
 
     String name;
 
