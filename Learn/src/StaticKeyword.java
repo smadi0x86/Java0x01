32ba09301daaf47e810fb36b872d1111c96f6957
Yousinator <y.omusabeh@gmail.com>
2023-04-09 07:27:30 +0300

    Edited by: Yousinator

--- Learn/src/StaticKeyword.java
+++ Learn/src/StaticKeyword.java
@@ -1,27 +1,56 @@
+/*
+    Writer's notes
+    ======================
+    ! It is recommended to have the "Better Comments" extension while reading this file.
+    * This works simultaneously with "Friend.java" file.
+    
+*/
+
+/*
+    Explaining Static Keyword
+    ======================
+    ! Static keyword is used to create a class attribute.
+    ! Static =  A modifier single copy of a variable/method is created and shared.
+    ! Static variables are shared among all the objects, class owns the static member.
+    ! We can apply the static keyword to methods as well as shown in "Friend.java" file.
+
+*/
+
+/*
+    Explaining the code
+    ======================
+    ? 1 > friend1, friend2 and friend3 share the same numberOfFriends variable (A modifier single copy).
+    ? 2 > We can access the static variable using the object name or the class name, but its not recommended due to the confusion.
+    ? 3 > This is an example of a static method that we can use without creating an object, instead of using Math math = new Math(); .
+
+*/
+
+//! ------------------------------------------------------ The Code ------------------------------------------------------ //
+
 public class StaticKeyword {
     public static void learn(String[] args) {
-    // ! Static keyword is used to create a class attribute.
-    // ! Static =  A modifier single copy of a variable/method is created and shared.
-    // ! Static variables are shared among all the objects, class owns the static member.
-    // * This works simultaneously with "Friend.java" file.
 
-    Friend friend1 = new Friend("Yousef"); // ? friend1, friend2 and friend3 share the same numberOfFriends variable (A modifier single copy).
-    Friend friend2 = new Friend("Ahmed");
-    Friend friend3 = new Friend("Mohamed");
+        Friend friend1 = new Friend("Yousef"); // ? 1
+        Friend friend2 = new Friend("Ahmed");
+        Friend friend3 = new Friend("Mohamed");
 
-    System.out.println(Friend.numberOfFriends);
-    System.out.println(friend1.numberOfFriends); // ? we can access the static variable using the object name or the class name, but its not recommended due to the confusion.
-    System.out.println(friend2.numberOfFriends);
-    System.out.println(friend3.numberOfFriends);
+        System.out.println(Friend.numberOfFriends);
+        System.out.println(friend1.numberOfFriends); // ? 2
+        System.out.println(friend2.numberOfFriends);
+        System.out.println(friend3.numberOfFriends);
 
-        // * If we remove the static keyword in "Friend.java" from the numberOfFriends variable, we will get 3 different values for each object and we will have to access the variable using the object name.
+        /*
+            ! If we remove the static keyword in "Friend.java" from the numberOfFriends variable:
+                * > We will get 3 different values for each object and we will have to access the variable using the object name.
+        */
 
-        // ! We can apply the static keyword to methods as well as shown in "Friend.java" file.
-        // ! This method will return the number of friends that we have created:
+        /*
+            ! We can apply the static keyword to methods as well as shown in "Friend.java" file.
+            ! This method will return the number of friends that we have created:
+        */
 
         Friend.getNumberOfFriends();
 
-
-       // Math.round(x); // ? This is an example of a static method that we can use without creating an object, instead of using Math math = new Math();
+        // Math.round(x); // ? 3
     }
 }
--- Learn/src/SuperKeyword.java
+++ Learn/src/SuperKeyword.java
@@ -1,13 +1,36 @@
+/*
+    Writer's notes
+    ======================
+    ! It is recommended to have the "Better Comments" extension while reading this file.
+    * This works simultaneously with "Person.java" and "Hero.java" files.
+    
+*/
+
+/*
+    Explaining Super Keyword 
+    ======================
+    ! The super keyword is used to refer to the superclass (parent) of an object, to access the methods & variables of the superclass.
+    ! It's similar to the "this" keyword that refers to the class that "super" keyword refers to the parent class.
+    
+*/
+
+/*
+    Explaining the code
+    ======================
+    ? 1 > We create an instance from the class.
+    ? 2 > We use print its attributes.
+    
+*/
+
+//! ------------------------------------------------------ The Code ------------------------------------------------------ //
+
 public class SuperKeyword {
     public static void learn(String[] args) {
-        // ! The super keyword is used to refer to the superclass (parent) of an object, to access the methods & variables of the superclass.
-        // ! It's similar to the "this" keyword that refers to the class that "super" keyword refers to the parent class.
-        // * This works simultaneously with "Person.java" and "Hero.java" files.
 
-        Hero hero1 = new Hero("Superman", 30, "Flying");
+        Hero hero1 = new Hero("Superman", 30, "Flying"); // ? 1
         Hero hero2 = new Hero("Batman", 40, "Money");
 
-        System.out.println(hero1.name);
+        System.out.println(hero1.name); // ? 2
         System.out.println(hero1.age);
         System.out.println(hero1.power);
 
